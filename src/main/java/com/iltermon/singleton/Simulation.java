87faa942f40be4a90dcf7d0ccfda0f3186c9b8ca
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2021-10-31 23:15:04 +0100

    add Singleton pattern

---
+++ src/main/java/com/iltermon/singleton/Simulation.java
@@ -0,0 +1,9 @@
+public class Simulation {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println(singleton.hashCode());
+        System.out.println(singleton2.hashCode());
+    }
+}
